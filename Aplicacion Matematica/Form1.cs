ef0a9ae97aea11d5ec5659bc4f96668f41eae3d7
Eldar <Eldar@DESKTOP-BS4H5I9>
2022-04-11 23:19:23 -0600

    Quitando borde superior por defecto y agregando uno  provisional

--- Aplicacion Matematica/Form1.cs
+++ Aplicacion Matematica/Form1.cs
@@ -15,22 +15,57 @@ namespace Aplicacion_Matematica
         public Form1()
         {
             InitializeComponent();
+            this.Shown += CreateButtonDelegate;
+    
+
         }
+
+
+        private void CreateButtonDelegate(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Panel panel1 = new Panel();
+            this.Controls.Add(panel1);
+            panel1.Location = new Point(0, 0);
+            panel1.BackColor = Color.Black;
+            panel1.Size = new Size(Form1.ActiveForm.Size.Width, 35);
+            
+
+            Button newButton = new Button();
+            this.Controls.Add(newButton);
+            newButton.Text = "X";
+            newButton.BackColor = Color.AntiqueWhite;
+            newButton.Size = new Size(20, 20);
+            newButton.Location = new Point(Form1.ActiveForm.Size.Width - newButton.Size.Width, 1);
+            newButton.Click += new System.EventHandler(this.newButton_Click);
+
+
+            label1.Text = Form1.ActiveForm.Size.Width.ToString();
+
+            panel1.Controls.Add(newButton);
+        }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
             var a = 5;
             var b = 6;
 
             var c = a * b;
 
-            label1.Text = c.ToString();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-           
             
         }
+
+
+
+       private void newButton_Click(object sender, EventArgs e)
+       {
+            Close();
+       }
     }
 }
