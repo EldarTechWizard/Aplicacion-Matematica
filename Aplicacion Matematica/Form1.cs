7859118e173367c8df6dbdcea184192a19097991
Eldar <Eldar@DESKTOP-BS4H5I9>
2022-04-25 07:50:27 -0600

    AGREGANDO UNA ETIQUETA

--- Aplicacion Matematica/Form1.cs
+++ Aplicacion Matematica/Form1.cs
@@ -50,5 +50,10 @@ namespace Aplicacion_Matematica
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
