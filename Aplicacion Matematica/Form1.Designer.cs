233820d66b18d551d9a3a1e1e422c513003e699f
Eldar <Eldar@DESKTOP-BS4H5I9>
2022-04-24 21:31:21 -0600

    Agregando nuevo borde de arriba

--- Aplicacion Matematica/Form1.Designer.cs
+++ Aplicacion Matematica/Form1.Designer.cs
@@ -29,27 +29,53 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(397, 105);
+            this.label1.Location = new System.Drawing.Point(40, 43);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-      
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, -1);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(921, 29);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MovePanel);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(843, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(925, 450);
+            this.ClientSize = new System.Drawing.Size(921, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "             ";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +84,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Aplicacion Matematica/Form1.cs
+++ Aplicacion Matematica/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,51 +13,22 @@ namespace Aplicacion_Matematica
 {
     public partial class Form1 : Form
     {
+
+        [DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
         public Form1()
         {
             InitializeComponent();
-            this.Shown += CreateButtonDelegate;
-    
-
         }
-
-
-        private void CreateButtonDelegate(object sender, EventArgs e)
-        {
-            this.BackColor = Color.White;
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.StartPosition = FormStartPosition.CenterScreen;
-
-            Panel panel1 = new Panel();
-            this.Controls.Add(panel1);
-            panel1.Location = new Point(0, 0);
-            panel1.BackColor = Color.Black;
-            panel1.Size = new Size(Form1.ActiveForm.Size.Width, 35);
-            
-
-            Button newButton = new Button();
-            this.Controls.Add(newButton);
-            newButton.Text = "X";
-            newButton.BackColor = Color.AntiqueWhite;
-            newButton.Size = new Size(20, 20);
-            newButton.Location = new Point(Form1.ActiveForm.Size.Width - newButton.Size.Width, 1);
-            newButton.Click += new System.EventHandler(this.newButton_Click);
-
-
-            label1.Text = Form1.ActiveForm.Size.Width.ToString();
-
-            panel1.Controls.Add(newButton);
-        }
-
-
 
         private void Form1_Load(object sender, EventArgs e)
         {
             
-            var a = 5;
-            var b = 6;
-
-            var c = a + b;
+    
 
             
         }
@@ -67,5 +39,16 @@ namespace Aplicacion_Matematica
        {
             Close();
        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MovePanel(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
     }
 }
